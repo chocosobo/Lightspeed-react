a1bd399b36ca82d35fbb2a9b3dbe983dec5a5cac
chocosobo <jovehong@gmail.com>
2021-05-28 16:40:21 +0900

    make it good looking

--- src/styles/liveChatStyles.js
+++ src/styles/liveChatStyles.js
@@ -8,12 +8,24 @@ export const ChatContainer = styled.div`
   margin: 0 2.5em;
   min-width: 25em;
   width: 25%;
-  max-width: 600px
+  max-width: 600px;
+  height: 100%;
   
 
   @media only screen and (max-width: 1024px) {
     margin: 1em 0.3em;
     min-width: unset;
-    width: 100%
+    width: 100%;
+  }
+`;
+
+export const WidgetBot = styled.div`
+  width:100%;
+  height:100%;
+
+  @media only screen and (max-width: 1024px) {
+    margin: 1em 0.3em;
+    min-width: unset;
+    width: 91%;
   }
 `;
