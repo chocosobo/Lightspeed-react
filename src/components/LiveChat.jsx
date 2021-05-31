206451b7d1bc8c7e0f14107b0507b1355c1c5a35
chocosobo <jovehong@gmail.com>
2021-05-31 09:11:42 +0900

    edit

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -2,39 +2,8 @@ import * as React from 'react';
 import {
   ChatContainer
 } from "../styles/liveChatStyles";
-import WidgetBot from '@widgetbot/react-embed';
 
-class App extends React.Component {
-  api: API
-
-  onAPI(api: API) {
-    this.api = api
-    api.on('signIn', user => {
-      console.log(`Signed in as ${user.name}`, user)
-    })
-  }
-
-  handleClick() {
-    this.api.emit('sendMessage', `Hello world! from \`@widgetbot/react-embed\``)
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick.bind(this)}>
-          {`Send "Hello world"`}
-        </button>
-        <WidgetBot
-          server="847601882851704862"
-          channel="847601882851704865"
-          onAPI={this.onAPI.bind(this)}
-        />
-      </div>
-    )
-  }
-}
-
-export default WidgetBothawawa;
+import WidgetBothawawa from "./dico"
 
 
 
